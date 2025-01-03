85e9de9d7d01496aea987de8610a9e09b51c1b91
Jordan Alexander Unger <jaunger1999@gmail.com>
2025-01-03 01:10:54 +0000

    got rid of some unnecessary stuff

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,10 +1,8 @@
 return {
-	'echasnovski/mini.nvim', 
+	'echasnovski/mini.nvim',
 	enabled = true,
 	version = false,
 	config = function()
-		local statusline = require 'mini.statusline'
-		statusline.setup { use_icons = true }
 		require('mini.comment').setup()
 		require('mini.align').setup()
 	end
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -9,12 +9,8 @@ return {
     -- refer to the configuration section below
     bigfile = { enabled = true },
     dashboard = { enabled = true },
-    indent = { enabled = true },
     input = { enabled = true },
-    notifier = { enabled = true },
     quickfile = { enabled = true },
-    scroll = { enabled = true },
     statuscolumn = { enabled = true },
-    words = { enabled = true },
   },
 }
